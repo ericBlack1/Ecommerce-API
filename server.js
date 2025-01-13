34c640292bb02146052e2b5afea0be1dac677f67
ericBlack1 <ericangeloawa@gmail.com>
2025-01-13 23:21:19 +0100

    integration of swagger docs UI

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
 const connectDB = require('./config/db');
+const swaggerDocs = require('./config/swagger');
 
 dotenv.config();
 connectDB();
@@ -10,8 +10,9 @@ connectDB();
 const app = express();
 app.use(express.json());
 
+swaggerDocs(app);
+
 app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
