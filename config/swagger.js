b99c0d6f839d45cd218b72bd6284ba867cd2fc23
ericBlack1 <ericangeloawa@gmail.com>
2025-01-14 23:29:13 +0100

    product model created

--- config/swagger.js
+++ config/swagger.js
@@ -16,7 +16,7 @@ const options = {
             },
         ],
     },
-    apis: ['./routes/*.js'], // Path to the API docs
+    apis: ['./routes/*.js'],
 };
 
 const swaggerSpec = swaggerJsdoc(options);
