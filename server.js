b32e42dd37fc9e70a442a126c9d94d51b39cb97b
ericBlack1 <ericangeloawa@gmail.com>
2025-01-14 23:32:56 +0100

    update server.js for products

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
 const connectDB = require('./config/db');
 const swaggerDocs = require('./config/swagger');
+const productRoutes = require('./routes/productRoutes');
 
 dotenv.config();
 connectDB();
@@ -13,6 +14,7 @@ app.use(express.json());
 swaggerDocs(app);
 
 app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
